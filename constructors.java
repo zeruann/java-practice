60e945b3d12a4b162d492171f53bbb4fa36a9ba5
Hazel <hazelannmcarillo@gmail.com>
2025-07-02 14:30:57 +0800

    Create constructors.java

---
+++ constructors.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaoop;
+
+/**
+ *
+ * @author Hazel Ann
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        /* 
+        Human human1 = new Human("Rick",45,70);
+        Human human2 = new Human("Morty",57,50);
+        System.out.println(human1.name);
+        System.out.println(human2.name);
+        
+        human2.eat();
+        human1.drink();
+        
+        */
+
+      
+       /* public class Human {
+    
+
+      String name;
+      int age;
+      double weight;
+      
+      Human(String name, int age, double weight){
+          this.name = name;
+          this.age = age;
+          this.weight = weight;
+      }
+      
+      void eat(){
+          System.out.println(this.name+" is eating!");
+      }
+      void drink(){
+          System.out.println(this.name+" is drinking!");
+      }
+   
+      } */
+
+       
+        
+    }
+    
+}
