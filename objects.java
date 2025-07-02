012764d0b9b8478204f66364b98d9cd38fc40efa
Hazel <hazelannmcarillo@gmail.com>
2025-07-02 14:28:22 +0800

    Create objects.java

---
+++ objects.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaoop;
+
+/**
+ *
+ * @author Hazel Ann
+ */
+public class JavaOOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Objects
+        Car myCar1 = new Car();
+        Car myCar2 = new Car();
+        System.out.println(myCar1.make);
+        System.out.println(myCar1.model);
+        System.out.print("");
+        System.out.println(myCar2.make);
+        System.out.println(myCar2.model);
+        
+        //myCar.drive();
+        //myCar.brake();
+        */
+        
+    /*public class Car {
+     Objects
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2020;
+    String color = "blue";
+    double price = 50000.00;
+    
+
+    void drive(){
+        System.out.println("You drive the car");
+    }
+    void brake(){
+        System.out.println("You step on the brakes");
+            }
+    }*/
+        
+        
+    
+}
+}
