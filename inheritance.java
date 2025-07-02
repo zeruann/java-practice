b095a7dc46e2548445fef19515d80d66ba526f69
Hazel <hazelannmcarillo@gmail.com>
2025-07-02 14:34:32 +0800

    Create inheritance.java

---
+++ inheritance.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaoop;
+
+/**
+ *
+ * @author Hazel Ann
+ */
+
+
+/*
+public class Vehicle {
+
+    double speed;
+    
+    void go(){
+        System.out.println("This vehicle is moving");
+    }
+    void stop(){
+        System.out.println("This vehicle is stopped");
+    }
+   
+    
+}
+*/
+
+
+/*
+ public class Car extends Vehicle{
+ 
+    int wheels = 4;
+    int doors = 4;
+}
+*/
+
+/*
+public class Bicycle extends Vehicle{
+    
+    int wheels = 2;
+    int pedals = 2;
+
+}
+*/
+
+
+public class Main {
+    
+    public static void main(String[] args){
+        
+        /*
+        Car car = new Car();
+        
+        car.go();
+        
+        Bicycle bike = new Bicycle();
+        
+        bike.stop();
+        
+        System.out.println(car.doors);
+        System.out.println(bike.pedals);
+        */
+        
+    }
+    
+}
